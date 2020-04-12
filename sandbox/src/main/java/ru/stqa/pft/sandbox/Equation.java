9335eb8e1c0c11898f2efa18f7fbaff553791647
Kraven83 <63001456+Kraven83@users.noreply.github.com>
2020-04-12 04:55:22 +0300

    Добавлена возвожность выбора браузера.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -2,23 +2,33 @@ package ru.stqa.pft.sandbox;
 
 public class Equation {
 
-  private final double a;
-  private final double b;
-  private final double c;
+  private double a;
+  private double b;
+  private double c;
 
   private int n;
 
-  public Equation (double a, double b, double c) {
+  public Equation(double a, double b, double c) {
     this.a = a;
     this.b = b;
     this.c = c;
 
     double d = b * b - 4 * a * c;
 
-    if(d > 0) {
-      n = 2;
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
     } else {
-      if(d == 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
