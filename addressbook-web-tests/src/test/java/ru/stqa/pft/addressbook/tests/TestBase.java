4f9ac47d569d8f901123506cabfaa444b6269cfa
Kraven83 <63001456+Kraven83@users.noreply.github.com>
2021-06-02 23:05:31 +0300

    Задание 14. Формирование конфигурационного файла для запуска тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -9,7 +9,9 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  //protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME); //явное указание браузера
+  // -Dbrowser=firefox - параметр для запуска в FireFox
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME)); // браузер берется из параметров, по дефолту - Хром
 
   @BeforeSuite
   public void setUp() throws Exception {
